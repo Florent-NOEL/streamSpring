0d8612d4cbeaf0b085831f9274f223455c787d0f
NOEL Florent <florent.noel@soprasteria.com>
2023-10-26 15:13:43 +0200

    init git project

---
+++ streamSpring/src/main/java/streamSpring/streamSpring/StreamSpringApplication.java
@@ -0,0 +1,13 @@
+package streamSpring.streamSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamSpringApplication.class, args);
+	}
+
+}
