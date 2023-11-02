0febe16716b0e2187abba64de14cd442e41fc7e9
NOEL Florent <florent.noel@soprasteria.com>
2023-11-02 12:23:21 +0100

    last update 02/11/23

--- src/main/java/streamSpring/restcontrollers/VideoRestcontroller.java
+++ src/main/java/streamSpring/restcontrollers/VideoRestcontroller.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBui
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import streamSpring.dto.request.VideoRequest;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
@@ -34,7 +36,7 @@ public class VideoRestcontroller {
         try {
             // Récupérez le fichier téléchargé et enregistrez-le localement
             byte[] bytes = file.getBytes();
-            String fileName = "C:\\Users\\Flo\\Documents\\wb\\videoFile\\" + file.getOriginalFilename();
+            String fileName = "C:\\Users\\flnoel\\Desktop\\Test Developpement\\" + file.getOriginalFilename();
             FileOutputStream stream = new FileOutputStream(fileName);
             stream.write(bytes);
             stream.close();
@@ -44,53 +46,34 @@ public class VideoRestcontroller {
         }
 
     }
+    @PostMapping("/uploadImage")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String uploadImage(@RequestParam("file") MultipartFile fileMultipart) {
+        String status = "faild to upload image";
 
-    @PostMapping("/getVideo")
-    public String getVideo(@RequestBody VideoRequest videoRequest) {
-        String path = videoRequest.getPath();
-        String type = videoRequest.getType();
-        path = "C://Users//Flo//Documents//wb//videoFile//hero.webm";
-        type = "video/webm";
-        System.out.println(path);
-        System.out.println(type);
-        // Load and return the video file as a Resource.
-        Resource videoResource = new FileSystemResource(path);
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path(path)
-                .toUriString();
-
-        return fileDownloadUri;
-    }
-
-    // Répertoire local où se trouve le fichier vidéo
-    private static final String videoDirectory = "C://Users//Flo//Documents//wb//videoFile//";
-
-    @GetMapping("/video/{videoFileName}")
-    public ResponseEntity<Resource> serveVideo(@PathVariable String videoFileName) {
         try {
-            // Construire le chemin complet du fichier vidéo
-            Path videoPath = Paths.get(videoDirectory).resolve(videoFileName).normalize();
-            Resource resource = new UrlResource(videoPath.toUri());
 
-            if (resource.exists() || resource.isReadable()) {
-                return ResponseEntity.ok()
-                        .body(resource);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (MalformedURLException e) {
-            return ResponseEntity.notFound().build();
+            BufferedImage image = ImageIO.read(fileMultipart.getInputStream());
+            String filepath = "C:\\Users\\flnoel\\Desktop\\Test Developpement\\" + fileMultipart.getName();
+            File outputFile = new File(filepath);
+            ImageIO.write(image, "jpg", outputFile);
+            // Récupérez le fichier téléchargé et enregistrez-le localement
+            System.out.println("L'image a été sauvegardée avec succès dans : " + filepath);
+            status = ("L'image a été sauvegardée avec succès dans : " + filepath);
         }
+
+
+        catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Erreur lors de la sauvegarde de l'image.");
+        }
+        return status;
+
     }
 
-    // Générez une URL pour lire la vidéo
-    @GetMapping("/videoURL/{videoFileName}")
-    public String getVideoURL(@PathVariable String videoFileName) {
-        String videoUrl = MvcUriComponentsBuilder
-                .fromMethodName(VideoRestcontroller.class, "serveVideo", videoFileName)
-                .build()
-                .toUriString();
-        return videoUrl;
     }
 
-}
+
+
+
+
